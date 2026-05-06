87f172f540a32a854c94c68f6df85146b1fac757
juandiegogalindo <jdgalindo6@gmail.com>
2026-05-05 23:16:43 -0500

    CRUD estacion de servicio, completa

--- src/java/persistencia/EstacionDAO.java
+++ src/java/persistencia/EstacionDAO.java
@@ -13,11 +13,10 @@ import java.util.List;
  *
  * @author jdgal
  */
-
 public class EstacionDAO {
 
-    private EntityManagerFactory emf =
-            Persistence.createEntityManagerFactory("CombustiblePU");
+    private EntityManagerFactory emf
+            = Persistence.createEntityManagerFactory("CombustiblePU");
 
     public void create(EstacionServicio e) {
         EntityManager em = emf.createEntityManager();
@@ -31,5 +30,29 @@ public class EstacionDAO {
         EntityManager em = emf.createEntityManager();
         return em.createQuery("SELECT e FROM EstacionServicio e", EstacionServicio.class)
                 .getResultList();
+    }
+
+    public EstacionServicio findById(Long id) {
+        EntityManager em = emf.createEntityManager();
+        return em.find(EstacionServicio.class, id);
+    }
+
+    public void update(EstacionServicio e) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.merge(e);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    public void delete(Long id) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        EstacionServicio e = em.find(EstacionServicio.class, id);
+        if (e != null) {
+            em.remove(e);
+        }
+        em.getTransaction().commit();
+        em.close();
     }
 }
--- src/java/servicios/EstacionService.java
+++ src/java/servicios/EstacionService.java
@@ -29,9 +29,50 @@ public class EstacionService {
         return dao.findAll();
     }
 
+    @GET
+    @Path("/{id}")
+    public Response getById(@PathParam("id") Long id) {
+        EstacionServicio e = dao.findById(id);
+        if (e == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        return Response.ok(e).build();
+    }
+
     @POST
     public Response create(EstacionServicio e) {
         dao.create(e);
         return Response.status(Response.Status.CREATED).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Response update(@PathParam("id") Long id, EstacionServicio e) {
+        EstacionServicio existente = dao.findById(id);
+
+        if (existente == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        existente.setNombre(e.getNombre());
+        existente.setUbicacion(e.getUbicacion());
+
+        dao.update(existente);
+
+        return Response.ok().build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response delete(@PathParam("id") Long id) {
+        EstacionServicio existente = dao.findById(id);
+
+        if (existente == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        dao.delete(id);
+
+        return Response.ok().build();
     }
 }
